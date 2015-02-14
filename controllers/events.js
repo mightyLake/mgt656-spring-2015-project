5d0e8f86aa8116fdf1f55a4239a2bdcac2afaf37
F. Anthony Annetta <francesco.annetta@gmail.com>
2015-02-14 21:04:52 +0000

    modity event creation form to use Kyle's method

--- controllers/events.js
+++ controllers/events.js
@@ -27,7 +27,8 @@ var allowedDateInfo = {
   hours: [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
     12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
-  ]
+  ],
+  years: [2015,2016]
 };
 
 /**
@@ -47,7 +48,7 @@ function listEvents(request, response) {
  * Controller that renders a page for creating new events.
  */
 function newEvent(request, response){
-  var contextData = {};
+  var contextData = {allowedDateInfo: allowedDateInfo};
   response.render('create-event.html', contextData);
 }
 
